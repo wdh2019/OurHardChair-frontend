a479e422e5a455422eeafba5d02fd60ec4c6ef45
wangdonghui <18302010012@fudan.edu.cn>
2020-03-17 21:34:03 +0800

    2020/3/17 21:33

---
+++ frontend/README.md
@@ -0,0 +1,56 @@
+# frontend
+
+> A Vue.js project
+
+## 2020/3/17  21:17 王栋辉著
+
+第一次前端改动包括：
+
+删除HelloWorld.vue，添加了会议申请相关的MeetingApply.vue
+
+template部分，基本修改完成
+
+script部分仍有问题：表单的前端验证尚未填完，$axios.post方法尚未搞懂
+
+css部分没有完成
+
+提示：
+
+需要修改的文件(.vue)在components中找到，建议按照官网教程，下载HBuilderX编辑器。
+
+关于vue框架的文档结构，附上两篇文章参考：
+
+https://www.cnblogs.com/yyh1993/p/5846586.html
+https://www.cnblogs.com/yyh1993/p/5847230.html
+
+请尽量不要动三个vue文件之外的文件，尚未搞清其具体用途。
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+
+
---
+++ frontend/src/components/Register.vue
@@ -0,0 +1,147 @@
+<template>
+  <div id="base_register">
+
+    <el-form :model="registerForm" :rules="rules" class="register_container" label-position="left"
+             label-width="0px" v-loading="loading" :ref="registerForm">
+      //标题
+      <h3 class="register_title">注册Register</h3>
+      //用户名
+      <el-form-item prop="username">
+        <el-input type="text" v-model="registerForm.username"
+                  auto-complete="off" placeholder="username"></el-input>
+      </el-form-item>
+      //密码
+      <el-form-item prop="password">
+        <el-input type="password" v-model="registerForm.password"
+                  auto-complete="off" placeholder="password"></el-input>
+      </el-form-item>
+      //邮箱
+      <el-form-item prop="email">
+        <el-input type="text" v-model="registerForm.email"
+                  auto-complete="off" placeholder="email"></el-input>
+      </el-form-item>
+      //所属单位
+      <el-form-item prop="company">
+        <el-input type="text" v-model="registerForm.company"
+                  auto-complete="off" placeholder="company"></el-input>
+      </el-form-item>
+      //所在区域
+      <el-form-item prop="district">
+        <el-input type="text" v-model="registerForm.district"
+                  auto-complete="off" placeholder="district"></el-input>
+      </el-form-item>
+      //用户类型（admin,contributor或reviewer)
+      <el-form-item prop="usertype">
+        <el-radio-group v-model="registerForm.usertype" @change="userTypeChange">
+          <el-radio label="Admin" border>Admin</el-radio>
+          <el-radio label="Contributor" border>Contributor</el-radio>
+          <el-radio label="Reviewer" border>Reviewer</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      //提交按钮
+      <el-form-item style="width: 100%">
+        <el-button type="primary" style="width: 40%;background: #afb4db;border: none" v-on:click="register(registerForm)">register</el-button>
+      </el-form-item>
+
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data () {
+    const dataValid = (rule, value, callback) => {
+      //未填写信息的处理
+      if(!value || value === '') {
+        return callback(new Error('Can\'t be empty'))
+      }
+
+      return callback()
+    }
+    return {
+      registerForm: {
+        username: '',
+        password: '',
+        email: '',
+        company:'',
+        district:'',
+        usertype: ''
+      },
+      rules: {
+        // blur 失去鼠标焦点时触发验证
+        username: [{required: true, message: '', trigger: 'blur'}, {validator: dataValid, trigger: 'blur'}],
+        password: [{required: true, message: '', trigger: 'blur'}, {validator: dataValid, trigger: 'blur'}],
+        email:    [{required: true, message: '', trigger: 'blur'}, {validator: dataValid, trigger: 'blur'}],
+        company:  [{required: true, message: '', trigger: 'blur'}, {validator: dataValid, trigger: 'blur'}],
+        district: [{required: true, message: '', trigger: 'blur'}, {validator: dataValid, trigger: 'blur'}],
+        usertype: [{required: true, message: '', trigger: 'blur'}, {validator: dataValid, trigger: 'blur'}]
+      },
+      loading: false
+    }
+  },
+
+  methods: {
+    userTypeChange() {    },
+    //表单验证
+    register (formName) {
+      this.$refs[formName].validate(valid => {
+        if(valid){
+          //this.$axios.post用来向后台请求数据
+          this.$axios.post('/register', {
+              username: this.registerForm.username,
+              password: this.registerForm.password,
+              email: this.registerForm.email,
+              company: this.registerForm.company,
+              district:this.registerForm.district,
+              authorities: [this.registerForm.usertype]
+            }
+          )
+            .then(resp => {
+              // 根据后端的返回数据修改
+              if(resp.status === 200 && resp.data.hasOwnProperty("id")) {
+                // 跳转到login
+                alert('注册成功！')
+                this.$router.replace('/login')
+              } else{
+                alert('注册失败！')
+              }
+            })
+            .catch(error => {
+              console.log(error)
+              alert('服务器后端出现错误！')
+            })
+        } else {
+          alert('您的填写有不正确的地方！')
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #base_register{
+    background: url("../assets/background/checkerboard-cross.png") repeat;
+    background-position: center;
+    height: 100%;
+    width: 100%;
+    background-size: cover;
+    position: fixed;
+  }
+  .register_container{
+    border-radius: 15px;
+    background-clip: padding-box;
+    margin: 90px auto;
+    width: 350px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+  }
+  .register_title{
+    margin: 0px auto 40px auto;
+    text-align: center;
+    color: #505458;
+  }
+</style>
