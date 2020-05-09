2b671ea889660d4dd79e1adcb8eeddc5bd2feaf6
YangYunteng <18302010022@fudan.edu.cn>
2020-05-10 00:03:50 +0800

    bug修复4-1

--- frontend/src/components/ConferenceForChair.vue
+++ frontend/src/components/ConferenceForChair.vue
@@ -349,7 +349,7 @@
                   message: resp.data.message,
                   type: 'success'
                 });
-                window.location.reload();
+                //window.location.reload();
               } else {
                 this.$message({
                   showClose: true,
