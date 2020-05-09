d02ff62de74bee019b0f53e41fde8ab99abffe63
wangdonghui <18302010012@fudan.edu.cn>
2020-05-09 21:45:37 +0800

    debug：确认conference_id是数字

--- frontend/src/components/ConferenceForChair.vue
+++ frontend/src/components/ConferenceForChair.vue
@@ -337,7 +337,7 @@
       },
       changeReleaseStatus(row) {
         console.log(row);
-        console.log(row.conference_id);
+        console.log(isNumber(conference_id));
         if (row.is_open_submission === 3) {
           this.$axios.post('/releaseReviewResult', {
             conference_id: row.conference_id,
