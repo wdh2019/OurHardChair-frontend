e104478f7bfe15835c1ca4ae4a88dd89d66005bb
wangdonghui <18302010012@fudan.edu.cn>
2020-06-13 21:03:39 +0800

    补充网页拦截

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -151,7 +151,7 @@ export const router = new Router({
       name: '其他功能',
       component: UserPage,
       meta: {
-        //requireAuth: true,  // 添加该字段，表示进入这个路由是需要登录的
+        requireAuth: true,  // 添加该字段，表示进入这个路由是需要登录的
       },
       children: [
         {
