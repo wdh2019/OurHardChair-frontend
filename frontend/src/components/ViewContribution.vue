6acea8488242760f84cee973f2d283a10a0fb678
YangYunteng <18302010022@fudan.edu.cn>
2020-06-14 15:52:08 +0800

    测试预览

--- frontend/src/components/ViewContribution.vue
+++ frontend/src/components/ViewContribution.vue
@@ -194,6 +194,8 @@
         return {status: _status, type: type};
       },
       viewPDF(conference_id, title) {
+        console.log("http://114.116.112.8:8080/js/pdf/web/viewer.html?file="
+          + encodeURIComponent("/preview/" + conference_id + "/" + title));
         window.open(
           "http://114.116.112.8:8080/js/pdf/web/viewer.html?file="
           + encodeURIComponent("/preview/" + conference_id + "/" + title));
@@ -250,7 +252,7 @@
       },
       /* 确认评审结果 */
       confirmResult(row) {
-        this.$axios.post('/confirmReviewResult/'+this.$route.params.conference_id+"/"+row.articleId+"/"+this.$store.state.id)
+        this.$axios.post('/confirmReviewResult/' + this.$route.params.conference_id + "/" + row.articleId + "/" + this.$store.state.id)
           .then(resp => {
             this.$message({
               showClose: true,
